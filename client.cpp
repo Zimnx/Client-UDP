88bd93f7900a210ecb9bfa0b287d64db97704bdb
Zimny <mzimnoch@hotmail.com>
2013-04-29 17:14:44 +0200

    Second testing version, increased number of packets beign sent during one request, fixed output

--- client.cpp
+++ client.cpp
@@ -1,17 +1,16 @@
-/* 
- * Klient UDP
- */
+// Maciej Zimnoch - 248104
+// Sieci Komputerowe - Pracownia 2
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <map>
-#include <queue>
+#include <algorithm>
 #include <string>
 #include <cstring>
 #include <vector>
- #include <fstream>
+#include <fstream>
 #include <sstream>
 #include "sockwrap.h"
 
@@ -39,21 +38,14 @@ string filename;
 int bytes;
 char address[] = "156.17.4.30"; // aisd.ii.uni.wroc.pl
 
-int requestsAtOnce = 100;
+int requestsAtOnce = 20;
 map<int,wrapper> results;
 
 void sendDatagram(int sockfd, sockaddr_in server_address, int start, int bytes);
-string getStart(string info);
+string getStart(char* buffer);
 void deleteRequest(int start);
-char* getData(char* buffer, int size);
-void print_bytes (int count,char* buffer_ptr)
-{
-    for (int i=0; i<count; i++) {
-        printf ("%.2x ", *buffer_ptr);
-        buffer_ptr++;
-    }
-    printf("\n");
-}
+int findChar(const char* buffer,int size, char c);
+
 int main (int argc, char** argv)
 {
     if(argc != 4) { printf("Usage: client-udp <port> <output> <bytes>\n"); exit(1);}
@@ -61,7 +53,7 @@ int main (int argc, char** argv)
     port = atoi(argv[1]);
     filename = string(argv[2]);
     bytes = atoi(argv[3]);
-
+    int totalRequests = bytes/pack + 1;
     int sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
 
     struct sockaddr_in server_address;
@@ -69,12 +61,10 @@ int main (int argc, char** argv)
     server_address.sin_family   = AF_INET;
     server_address.sin_port     = htons(port);
     inet_pton(AF_INET, address, &server_address.sin_addr); 
-    
-    for(int i = 0; i < (bytes/pack) + 1 ; ++ i)
+    for(int i = 0; i < totalRequests ; ++ i)
     {
         int start = i*pack;
         requests.push_back(start);
-
     }
 
     while(requests.size() > 0)
@@ -87,10 +77,12 @@ int main (int argc, char** argv)
                 int requestBytes = (bytes - start >= pack) ? pack : (bytes - start);
                 if(requestBytes == 0)
                     deleteRequest(start);
-                sendDatagram(sockfd, server_address, start, requestBytes);
+                for(int j = 0 ; j < 6 ; ++j)
+                    sendDatagram(sockfd, server_address, start, requestBytes);
             }
             
         }
+        int got = 0;
         while(1)
         {
             sockaddr_in client_address;
@@ -100,47 +92,43 @@ int main (int argc, char** argv)
             
             FD_ZERO(&descriptors);
             FD_SET(sockfd,&descriptors);    
-            timeval timeout; timeout.tv_sec = 0; timeout.tv_usec = 100000;
+            timeval timeout; timeout.tv_sec = 1; timeout.tv_usec = 0;
             int ready = Select(sockfd+1, &descriptors, NULL, NULL, &timeout);
-            if(ready)
+            if(!ready)
             {
-                char buffer[MAXMSG+1];
-                char* buffer_ptr = buffer;
-                Recvfrom (sockfd, buffer, MAXMSG, 0, &client_address, &len);
+                break;
+            }            
+            char buffer[MAXMSG+1];
+            char* buffer_ptr = buffer;
+            int n = Recvfrom (sockfd, buffer, MAXMSG, 0, &client_address, &len);
+            char ip_address[20];
+            inet_ntop (AF_INET, &client_address.sin_addr, ip_address, sizeof(ip_address));
+            string ip = string(ip_address);
+
+            if(strcmp(address,ip.c_str()) == 0)
+            {
                 
-                string buf = string(buffer);
-                int nl = buf.find_first_of('\n');
-                string info = buf.substr(0,nl+1);
+                int nl = findChar(buffer,n,'\n');
+                char* info = new char[nl+2];
+                strncpy(info,buffer,nl+2);
                 int start = atoi(getStart(info).c_str());
+                if(find(requests.begin(), requests.end(), start) == requests.end())
+                    continue;
                 int requestBytes = (bytes - start >= pack) ? pack : (bytes - start);
-                char *data = new char[requestBytes];
+                
                 for(int i = 0 ; i <= nl ; ++i)
                 {
                     buffer_ptr++;
                 }
-                //copy(buffer_ptr,buffer_ptr + pack, data);
-                //strcpy(data,buffer_ptr);
-                //strncat(data,buffer[nl+1],pack);
-                //strncat(data,buffer[nl+1],pack);
-                memcpy(data,buffer_ptr,requestBytes);
-
-                char ip_address[20];
-                inet_ntop (AF_INET, &client_address.sin_addr, ip_address, sizeof(ip_address));
-                string ip = string(ip_address);
-                if(strcmp(address,ip.c_str()) == 0)
-                {
-                    
-                    results[start] = wrapper(data,requestBytes);
-                    deleteRequest(start);
-                   // printf("got:%d %d %d %d %d | %s\n",start,n,nl,sizeof(data),sizeof(buffer),info.c_str());
-                    //print_bytes(pack, data);
-                }
-
+                wrapper data;
+                memcpy(data.tab,buffer_ptr,requestBytes);                
+                
+                results[start] = data;
+                printf("done %d %% \n",(got*100)/totalRequests);
+                got++;
+                deleteRequest(start);
             }
-            else
-            {
-                break;
-            }
+
         }
     }
     FILE* out;
@@ -150,8 +138,7 @@ int main (int argc, char** argv)
     {
         char* buffer = (*i).second.tab;
         int start = (*i).first;
-        int requestBytes = (bytes - start >= pack) ? pack : (bytes - start);//(*i).first;
-        //printf("sizeof(buffer)=%d bytes=%d",sizeof(buffer),requestBytes);
+        int requestBytes = (bytes - start >= pack) ? pack : (bytes - start);
         fwrite(buffer,1,requestBytes,out);
     }
     return 0;
@@ -164,13 +151,14 @@ void sendDatagram(int sockfd, sockaddr_in server_address, int start, int bytes)
     ostringstream ss;
     ss << "GET " << start << " " << bytes << "\n";
     string request = ss.str();
-    printf("sending: %s",request.c_str());
+   // printf("sending: %s",request.c_str());
     Sendto(sockfd, request.c_str(), strlen(request.c_str()),
             0, &server_address, sizeof(server_address));
 }
 
-string getStart(string info)
+string getStart(char* buffer)
 {
+    string info(buffer);
     int firstSpace = info.find_first_of(' ');
     int lastSpace = info.find_last_of(' ');
     return info.substr(firstSpace + 1,lastSpace - firstSpace - 1);
@@ -187,12 +175,12 @@ void deleteRequest(int start)
         }
     }
 }
-char* getData(char* buffer, int size)
+int findChar(const char* buffer,int size, char c)
 {
-    char* result = new char[size];
-    int i = 0;
-    while(buffer[i++] != '\n');
-    for(int j = 0 ; i < size; ++i,j++)
-        result[j] = buffer[i];
-    return result;
+    for(int i = 0 ; i < size ; ++i)
+    {
+        if(buffer[i] == c)
+            return i;
+    }
+    return -1;
 }
